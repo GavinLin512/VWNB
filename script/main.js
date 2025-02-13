2660299cb014cff67330c1fd0acf0c6f31811e5c
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2025-02-13 12:55:20 +0800

    change color

--- script/main.js
+++ script/main.js
@@ -222,7 +222,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#E3242B",
         ease: Expo.easeOut,
       },
       0.1,
